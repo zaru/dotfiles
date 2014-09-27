04a143e7bb3124104844ea5bea4f1082f67c4d2b
zaru@sakuraba <zarutofu@gmail.com>
2014-09-27 17:36:03 +0900

    update

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/oh-my-zsh ~/.oh-my-zsh
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/oh-my-zsh-powerline-theme/powerline.zsh-theme ~/.oh-my-zsh/themes/powerline.zsh-theme
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
